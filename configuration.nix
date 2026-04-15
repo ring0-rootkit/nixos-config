65752f2af6adec127c50dfaba95aacdd5c4d7491
Dmitry Dubina <git.ring0.rootkit@gmail.com>
2026-04-15 19:21:06 +0300

    light theme

--- configuration.nix
+++ configuration.nix
@@ -94,7 +94,7 @@ in
     dconf = {
       enable = true;
       settings = {
-        "org/gnome/desktop/interface".color-scheme = "prefer-dark";
+        "org/gnome/desktop/interface".color-scheme = "prefer-light";
         "org/gnome/shell" = {
           disable-user-extensions = false;
           enabled-extensions = with pkgs.gnomeExtensions; [
